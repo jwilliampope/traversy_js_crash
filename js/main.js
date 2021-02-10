9691c89aacea879300f56bc0035d9dd2b221f47b
jwilliampope <jwilliampope@hotmail.com>
2021-02-09 22:12:30 -0500

    traversy js crash course for practice

---
+++ js/main.js
@@ -0,0 +1,128 @@
+/* TIMESTAMP 08:00  */
+
+// alert("HAHAHAHAHA")
+
+// =======================
+
+/* 09:20 CONSOLE  */
+
+// console.log("HAHAHAHA")
+// console.error("ERROR")
+// console.warn("WARNING")
+
+// =======================
+
+// VARIABLES
+
+//***var***, let, const
+
+// let age = 30
+// age = 31
+// console.log(age)
+
+// .......................
+
+// let score
+// score = 10
+// console.log(score)
+
+// .......................
+
+// PRODUCES ERROR:
+
+// const score
+// console.log(score)
+
+// FIXED:
+
+// const score = 12
+// console.log(score)
+
+// =======================
+
+// DATA TYPES
+// string number boolean null undefined symbol
+
+// const firstName = "JOHN"
+// const age = 45
+// const rating = 7.8
+// const isCool = true
+// const x = null
+// const y = undefined
+// let z
+
+// console.log(typeof firstName) // yields string
+// console.log(typeof age) // yields number
+// console.log(typeof x) // HEADS UP - This is a JS bug - typeof will result in "object"
+// console.log(typeof z)
+
+// etc
+
+// STRING CONCATENTAION
+
+// const firstName = "John"
+// const age = 45
+// const rating = 7.8
+// const isCool = true
+// const x = null
+// const y = undefined
+// let z
+
+// console.log("My name is " + firstName + " and I am " + age + " years old.")
+
+// USING TEMPLATE LITERAL :
+// console.log(`My name is ${firstName} and I am ${age} years old.`)
+
+// ASSIGNED TO VARIABLE:
+// const hello = `My name is ${firstName} and I am ${age} years old.`
+// console.log(hello)
+
+// STRING PROPERTIES AND METHODS:
+
+// PROPERTY: length:
+// const s = "Hello World"
+// console.log(s.length)
+
+// METHOD: toUpperCase
+// const s = "Hello World"
+// console.log(s.toUpperCase())
+
+// ----------------------
+
+// substring
+// const s = "Hello World"
+// console.log(s.substring(0, 5))
+
+// combine methods :
+// const s = "Hello World"
+// console.log(s.substring(0, 5).toUpperCase())
+
+// (  try toLowerCase  )
+
+// ---------------------------
+
+// split ("splits" string into an array):
+
+// const s = "Hello World"
+// console.log(s.split())
+// returns ['Hello World']
+
+// -----------------------------------
+
+// const s = "Hello World"
+// console.log(s.split(""))
+// returns ['H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd']
+
+// -------------------
+
+// const s = "Hello World"
+// console.log(s.split(" "))
+// returns ['Hello', 'World']
+
+// -----------------------------
+
+// const s = "technology, computers, IT, code"
+// console.log(s.split(", "))
+// returns ['technology', 'computers', 'IT', 'code']
+
+// TIMESTAMP 24:00
